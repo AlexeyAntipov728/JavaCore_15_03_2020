8a44afca6bcb866be824e6bbcd336d042be2631f
AlexeyAntipov728 <62210339+AlexeyAntipov728@users.noreply.github.com>
2020-03-16 22:35:02 +0300

    Update

---
+++ src/HomeWork/lesson_1/All_in_One.java
@@ -0,0 +1,17 @@
+package HomeWork.lesson_1;
+
+import HomeWork.lesson_1.Task3.Calculate;
+import HomeWork.lesson_1.Task4.Compare;
+import HomeWork.lesson_1.Task5.PositiveOrNegative;
+import HomeWork.lesson_1.Task6.Negative;
+import HomeWork.lesson_1.Task7.HelloName;
+
+public class All_in_One {
+    public static void main(String[] args) {
+        Calculate.result(1,1,1,1); // не понимаю, почему не работает
+        Compare.compare(23,21); // не понимаю, почему не работает
+        PositiveOrNegative.isPositiveOrNegative(3);
+        Negative.isNegative(2); // не понимаю, почему не работает
+        HelloName.helloName("alexey");
+    }
+}
