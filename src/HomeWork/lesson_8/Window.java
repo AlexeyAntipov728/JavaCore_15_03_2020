942971fa835460e9158cd4cde8af88e1365837c3
AlexeyAntipov728 <62210339+AlexeyAntipov728@users.noreply.github.com>
2020-04-09 21:54:41 +0300

    HomeWork4/ GameXO

--- src/HomeWork/lesson_8/Window.java
+++ src/HomeWork/lesson_8/Window.java
@@ -1,105 +1,81 @@
 package HomeWork.lesson_8;
 
-import org.jetbrains.annotations.NotNull;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JFrame {
-    private ImageIcon imageX = new ImageIcon("src/HomeWork/lesson_8/X.jpg");
+    public ImageIcon imageX = new ImageIcon("src/HomeWork/lesson_8/X.jpg");
     private ImageIcon imageO = new ImageIcon("src/HomeWork/lesson_8/О.jpg");
     private ImageIcon imageEmpty = new ImageIcon("src/HomeWork/lesson_8/Empty.jpg");
     public int cnt = 0;
 
     public Window() {
 
-
         setTitle("Крестини - Нолики");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(300, 340);
         setLocation(700, 300);
-
 
         JPanel panelInfo = new JPanel(new BorderLayout(40, 300));
         JLabel score = new JLabel("Score: " + cnt);
         add(score, BorderLayout.NORTH);
         add(panelInfo);
 
-
         JPanel panel = new JPanel(new GridLayout(3, 3));
-
         JButton[][] buttons = new JButton[3][3];
+        System.out.println(imageX);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
 
+                buttons[i][j] = new JButton();
+                buttons[i][j].setIcon(imageEmpty);
+                JButton copy = buttons[i][j];
 
-            for (int i = 0; i < buttons.length; i++) {
-                for (int j = 0; j < buttons.length; j++) {
-                    buttons[i][j] = new JButton();
-                    buttons[i][j].setIcon(imageEmpty);
-                    panel.add(buttons[i][j]);
+                copy.addActionListener(action -> {
+                    copy.setIcon(imageX);
 
-                    JButton copy = buttons[i][j];
-                    copy.addActionListener(action -> {
-                        copy.setIcon(imageX);
-                        copy.setEnabled(false);
-                        copy.setDisabledIcon(imageX);
-
-                        movePC(buttons);
-
-                        if (isVictory(buttons, copy)) {
+                    copy.setEnabled(false);
+                    cnt++;
+                    copy.setDisabledIcon(imageX);
+                        if(isEmpty(buttons)){
+                            System.out.println("Ничья");
+                        }
+                        if(isVictory(buttons)){
+                            System.out.println("WIN WIN WIN WIN");
                             cnt++;
 
-                            System.out.println("Победа"); // может хоть в консоль выведет, но нет
-
-                            JFrame alert = new JFrame("Вы победили");
+                            JFrame alert = new JFrame("кто то победил или ничья");
                             alert.setLocation(500, 400);
                             alert.setSize(300, 100);
                             JPanel alertPanel = new JPanel(new FlowLayout());
+                            JButton newGame = new JButton("new game");
+                            newGame.addActionListener(a -> {
+                                this.dispose();
+
+                                alert.dispose();
+                            });
+                            JButton close = new JButton("close");
+                            close.addActionListener(a -> {
+                                dispose();
+                                alert.dispose();
+                            });
+                            alertPanel.add(newGame);
+                            alertPanel.add(close);
                             alert.add(alertPanel);
                             alert.setResizable(false);
                             alert.setVisible(true);
-
-
-
                         }
-
-                        if (isEmpty(buttons)) {
-                            JFrame alert = new JFrame("Ничья");
-                            alert.setLocation(500, 400);
-                            alert.setSize(300, 100);
-
-                        }
+                    movePC(buttons);
                 });
                 panel.add(buttons[i][j]);
-
-                }
-            }
-            add(panel);
-            setVisible(true);
-
-
-
-
-
-    }
-
-    public void movePC(JButton[][] buttons) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-               JButton pc = buttons[i][j];
-
-                if (pc.isEnabled()) {
-
-                    pc.setIcon(imageO);
-                    pc.setEnabled(false);
-                    pc.setDisabledIcon(imageO);
-
-                    return;
-                }
             }
         }
+        add(panel);
+        setVisible(true);
     }
 
-    private boolean isEmpty(JButton[][] buttons) {
+
+    private boolean isEmpty(JButton [][] buttons) {
         for (int i = 0; i < buttons.length; i++) {
             for (int j = 0; j < buttons.length; j++) {
                 if (buttons[i][j].isEnabled())
@@ -110,38 +86,61 @@ public class Window extends JFrame {
     }
 
 
-    public boolean isVictory(@NotNull JButton[][] buttons, JButton copy) {
-        //Проверка линий по диагоналям
-        boolean rightDiagonal, leftDiagonal;
-        rightDiagonal = true;
-        leftDiagonal = true;
+    public boolean isVictory(JButton [][] buttons) {
+
+        // Счетчик комбинаций по строке
+        int countRow = 0;
+        // Счетчик комбинаций по столбцу
+        int countColumn = 0;
+        // Счетчик комбинаций по первой диагонали
+        int countDiagonal1 = 0;
+        // Счетчик комбинаций по второй диагонали
+        int countDiagonal2 = 0;
+        // Проверка по строкам и столбцам
         for (int i = 0; i < buttons.length; i++) {
-            rightDiagonal &= (buttons[i][i] == copy(imageX));
-            leftDiagonal &= (buttons[buttons.length - i - 1][i] == copy(imageX));
-        }
-
-        if (rightDiagonal || leftDiagonal)
-            return true;
-        //Проверка столбцов и строк
-        boolean columns, lines;
-        for (int col = 0; col < buttons.length; col++) {
-            columns = true;
-            lines = true;
-            for (int lin = 0; lin < buttons.length; lin++) {
-                columns &= (buttons[col][lin] == copy(imageX));
-                lines &= (buttons[lin][col] == copy(imageX));
+            for (int j = 0; j < buttons.length; j++) {
+                if (buttons[i][j].getIcon(imageX)) countRow++;
+                if (buttons[j][i].getIcon(imageX)) countColumn++;
             }
-
-            if (columns || lines)
+            if (countRow == 3 || countColumn == 3) {
                 return true;
+            }
+            countRow = 0;
+            countColumn = 0;
         }
-
+        // Проверка по первой диагонали
+        for (int i = 0, j = 0; i < buttons.length; i++) {
+            if (buttons[i][j].getIcon(imageX)) countDiagonal1++;
+            j++;
+        }
+        if (countDiagonal1 == 3) {
+            return true;
+        }
+        // Проверка по второй диагонали
+        for (int i = 0, j = 2; i < buttons.length; i++) {
+            if (buttons[i][j].getIcon(imageX)) countDiagonal2++;
+            j--;
+        }
+        if (countDiagonal2 == 3) {
+            return true;
+        }
         return false;
+
     }
 
-    private JButton copy(ImageIcon imageX) {
-        return null;
-    }
+    private void movePC(JButton[][] buttons) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].isEnabled()) {
+                    buttons[i][j].setIcon(imageO);
+                    buttons[i][j].setEnabled(false);
+                    buttons[i][j].setDisabledIcon(imageO);
 
+                    cnt++;
+                    return;
+                }
+            }
+        }
+    }
 
 }
